6520611a1ffb9b79be23222e7a6c23e0674f320f
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-10 16:47:44 -0300

    Add authentication to send email

--- app/Http/Controllers/V1/MessageController.php
+++ app/Http/Controllers/V1/MessageController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
 use Dingo\Api\Exception\InternalHttpException;
+use Illuminate\Support\Facades\Auth;
 use Log;
+use App\User;
 
 class MessageController extends Controller
 {
@@ -20,7 +22,12 @@ class MessageController extends Controller
     {
         // Tries to send email
         try {
-            $this->api->post('api/v1/emails/send', $request->only('name', 'email', 'message'));
+            // Pretend to be authenticated user
+            $authUser = Auth::fromUser(User::first());
+
+            $this->api
+                ->be($authUser)
+                ->post('api/v1/emails/send', $request->only('name', 'email', 'message'));
         } catch (InternalHttpException $e) {
             Log::debug($e->getResponse(), ['file' => __FILE__, 'line' => __LINE__, 'method' => __METHOD__]);
             return $e->getResponse();
