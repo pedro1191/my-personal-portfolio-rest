e9c3d83aeee7decc3ac6f7fe7318400c3b67e5d6
pedro1191 <pedro_almeida1191@hotmail.com>
2019-04-10 14:19:40 -0300

    Add migration for projects table

---
+++ database/migrations/2019_04_08_234007_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->text('description');
+            $table->longText('image');
+            $table->string('live_demo_link', 100)->nullable();
+            $table->string('source_code_link', 100);
+            $table->integer('order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
