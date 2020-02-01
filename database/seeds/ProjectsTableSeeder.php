093f0042d4e0ce3b28e7c61aee8fdbfc63282e83
Pedro Almeida <pedro_almeida1191@hotmail.com>
2020-02-01 17:27:36 -0300

    Update projects table seeder

--- database/seeds/ProjectsTableSeeder.php
+++ database/seeds/ProjectsTableSeeder.php
@@ -13,17 +13,17 @@ class ProjectsTableSeeder extends Seeder
     public function run()
     {
         DB::table('projects')->insert([
-            'name' => 'My Products',
+            'name' => 'My Food',
             'description' => '<p>This is a sample application that shows products divided by category</p>',
             'image' => $this->getBase64Image('My_Food.jpg'),
-            'live_demo_link' => 'http://www.pedrohsalmeida.com:8088',
+            'live_demo_link' => 'https://www.pedrohsalmeida.com/my-food',
             'source_code_link' => 'https://github.com/pedro1191/my-products-vue',
             'order' => 1
         ]);
 
         DB::table('projects')->insert([
-            'name' => 'My Products API',
-            'description' => '<p>This is the RESTful API that My Products application communicates with</p>',
+            'name' => 'My Food API',
+            'description' => '<p>This is the RESTful API that My Food application communicates with</p>',
             'image' => $this->getBase64Image('RESTful_API_900x650.png'),
             'live_demo_link' => null,
             'source_code_link' => 'https://github.com/pedro1191/my-products-rest',
@@ -31,17 +31,17 @@ class ProjectsTableSeeder extends Seeder
         ]);
 
         DB::table('projects')->insert([
-            'name' => 'My Products Dashboard',
-            'description' => '<p>This is a dashboard application for the My Products application that allows to manage products and categories</p>',
+            'name' => 'My Food Dashboard',
+            'description' => '<p>This is a dashboard application for the My Food application that allows to manage products and categories</p>',
             'image' => $this->getBase64Image('My_Food_Dashboard.png'),
-            'live_demo_link' => 'http://www.pedrohsalmeida.com:8086',
+            'live_demo_link' => 'https://www.pedrohsalmeida.com/my-food-dashboard',
             'source_code_link' => 'https://github.com/pedro1191/my-products-dashboard-vue',
             'order' => 3
         ]);
 
         DB::table('projects')->insert([
-            'name' => 'My Products Dashboard API',
-            'description' => '<p>This is the RESTful API that My Products Dashboard application communicates with</p>',
+            'name' => 'My Food Dashboard API',
+            'description' => '<p>This is the RESTful API that My Food Dashboard application communicates with</p>',
             'image' => $this->getBase64Image('RESTful_API_900x650.png'),
             'live_demo_link' => null,
             'source_code_link' => 'https://github.com/pedro1191/my-products-dashboard-rest',
@@ -52,7 +52,7 @@ class ProjectsTableSeeder extends Seeder
             'name' => 'My Personal Portfolio',
             'description' => '<p>This is the application that you are currently in</p>',
             'image' => $this->getBase64Image('My_Personal_Portfolio.jpg'),
-            'live_demo_link' => 'http://www.pedrohsalmeida.com',
+            'live_demo_link' => 'https://www.pedrohsalmeida.com',
             'source_code_link' => 'https://github.com/pedro1191/my-personal-portfolio-vue',
             'order' => 5
         ]);
@@ -69,13 +69,13 @@ class ProjectsTableSeeder extends Seeder
 
     /**
      * Returns a image in base64
-     * 
+     *
      * @param  string  $imageName
      * @return string
      */
     private function getBase64Image(string $imageName)
     {
-        $path = storage_path('app/public/' . $imageName);
+        $path = resource_path('img/' . $imageName);
         $image = File::get($path);
         $contentType = File::mimeType($path);
 
